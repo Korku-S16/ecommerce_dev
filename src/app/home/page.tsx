ecf9d99c2f3a1bb0cd5e7e6f84f499389e07dd79
mayankjain2702 <jainayushi1202@gmail.com>
2025-05-08 14:21:34 +0530

    move the components

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,19 +1,15 @@
-import HeaderTop from "@/components/ui/HeaderTop";
-import Carousel from "@/components/ui/carousel";
-import Category from "@/components/ui/category";
+import Carousel from "@/app/home/component/carousel";
+import Category from "@/app/home/component/category";
 import Product from "@/components/ui/products";
 import Popular from "@/components/ui/popular";
-import Footer from "@/components/ui/footer";
 
 const YourPage = () => {
   return (
     <div>
-      <HeaderTop />
       <Carousel />
       <Category />
       <Product />
       <Popular />
-      <Footer />
     </div>
   );
 };
