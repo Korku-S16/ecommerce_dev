faffc602aaba396a642031dce31e478a3b081efa
mayankjain2702 <jainayushi1202@gmail.com>
2025-05-08 14:26:02 +0530

    changes import.

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import HeaderTop from "@/app/home/HeaderTop";
+import HeaderTop from "@/app/home/component/HeaderTop";
 import Footer from "@/app/home/component/footer";
 
 const geistSans = Geist({
