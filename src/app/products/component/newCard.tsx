0958f9fc58bb7ba70769c0174ceed79a9b07caae
mayankjain2702 <jainayushi1202@gmail.com>
2025-05-11 15:14:41 +0530

    product page.

---
+++ src/app/products/component/newCard.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { FiHeart } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
+interface ProductCardProps {
+  productCard: {
+    name: string;
+    price: number;
+    imageUrl: string;
+  };
+}
+
+function Card({ productCard }: ProductCardProps) {
+  return (
+    <div className="w-64 min-h-max min-w-max h-72 pt-[24px] scale-90 pr-[16px] pb-[24px] pl-[16px] bg-[#F6F6F6] rounded-sm text-center">
+      <div className="flex flex-col gap-3 ">
+        <div className="flex flex-row justify-between items-start ">
+          <FiHeart className="w-6 h-6" />
+          <Button variant="secondary" size="lg" className="cursor-pointer">
+            <MdDelete className="text-2xl" />
+          </Button>
+        </div>
+
+        <div className="flex items-center justify-center ">
+          <img src="/phone.png" className=" object-cover w-24 h-24  "></img>
+        </div>
+
+        <div className="flex flex-col gap-2 mt-2">
+          <p className="">Apple </p>
+          <p className=" text-2xl font-bold text-black">₹ 11111</p>
+        </div>
+        <Button>Buy Now</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,110 @@
-import ShoppingCart from "../cart/component/shoppingCart";
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import List from "./component/list";
+import Link from "next/link";
 
 const page = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 10;
+
+  interface GoToPageProps {
+    (pageNum: number): void;
+  }
+
+  const goToPage: GoToPageProps = (pageNum) => {
+    if (pageNum >= 1 && pageNum <= totalPages) {
+      setCurrentPage(pageNum);
+    }
+  };
+
   return (
-    <div>
-      <h2>Start here</h2>
+    <div className="flex flex-col min-h-screen px-4 py-4 md:px-8">
+      <div className="mb-4 text-sm text-gray-600 ml-4 md:ml-32">
+        <Link
+          href="/home"
+          className="hover:underline hover:text-black font-medium text-base"
+        >
+          Home
+        </Link>{" "}
+        <span className="mx-1">{">"}</span>
+        <span className="text-base font-medium text-black">
+          Mobiles & Tablets
+        </span>
+      </div>
+
+      <div className="flex flex-col lg:flex-row gap-6 flex-1 ml-4 md:ml-32">
+        <aside className="w-full lg:w-64">
+          <h2 className="text-lg font-semibold mb-2">Subcategories</h2>
+          <ul className="space-y-2 text-gray-700 text-sm">
+            <li>
+              <input type="checkbox" /> Apple
+            </li>
+            <li>
+              <input type="checkbox" /> Samsung
+            </li>
+            <li>
+              <input type="checkbox" /> Xiaomi
+            </li>
+            <li>
+              <input type="checkbox" /> OPPO
+            </li>
+            <li>
+              <input type="checkbox" /> Realme
+            </li>
+            <li>
+              <input type="checkbox" /> Motorola
+            </li>
+            <li>
+              <input type="checkbox" /> Nokia
+            </li>
+          </ul>
+        </aside>
+
+        <main className="flex-1 justify-center items-center mr-0 md:mr-32">
+          <List productList={[]} />
+
+          <div className="flex justify-center mt-6 space-x-2 items-center w-full">
+            <button
+              onClick={() => goToPage(currentPage - 1)}
+              disabled={currentPage === 1}
+              className="px-2 py-1 text-gray-600 hover:text-black disabled:opacity-50"
+            >
+              {"<"}
+            </button>
+
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((pageNum) => (
+              <button
+                key={pageNum}
+                onClick={() => goToPage(pageNum)}
+                className={`px-3 py-1 border rounded ${
+                  currentPage === pageNum
+                    ? "bg-black text-white"
+                    : "hover:bg-gray-200"
+                }`}
+              >
+                {pageNum}
+              </button>
+            ))}
+
+            <button
+              onClick={() => goToPage(10)}
+              className={`px-3 py-1 border rounded ${
+                currentPage === 10 ? "bg-black text-white" : "hover:bg-gray-200"
+              }`}
+            >
+              10
+            </button>
+
+            <button
+              onClick={() => goToPage(currentPage + 1)}
+              disabled={currentPage === totalPages}
+              className="px-2 py-1 text-gray-600 hover:text-black disabled:opacity-50"
+            >
+              {">"}
+            </button>
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
